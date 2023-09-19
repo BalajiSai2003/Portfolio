8816215a49357d92c5115274fe1038a1520c0c05
Balaji Sai Nadakuditi <104773951+BalajiSai2003@users.noreply.github.com>
2023-09-19 15:34:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -44,7 +44,7 @@ $(document).ready(function() {
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Problem Slover", "Developer"],
+        strings: ["Problem Solver", "Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
